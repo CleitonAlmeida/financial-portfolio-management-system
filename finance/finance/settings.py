545810bb4d98bed8ccc7f5dcea71ffe1a6442091
Cleiton Almeida <cleitonant@gmail.com>
2021-01-14 16:41:29 -0300

    fix settings

--- finance/finance/settings.py
+++ finance/finance/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    #'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
