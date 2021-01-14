49c8d6ba83280b4971f202a3cd3c317b16ecbe61
Cleiton Almeida <cleitonant@gmail.com>
2021-01-14 16:32:29 -0300

    Add WhiteNoise

--- finance/finance/settings.py
+++ finance/finance/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    #'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
