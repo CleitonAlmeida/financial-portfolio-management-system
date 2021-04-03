55cdfcfe2f5ff68e90f4fcddc27daf4340ca876f
Cleiton Almeida <cleitonant@gmail.com>
2021-04-03 15:58:56 -0300

    code refactoring (services utils)

--- portfolio/tests/utils.py
+++ portfolio/tests/utils.py
@@ -5,31 +5,33 @@ from portfolio.models import (
     Transaction
 )
 from django.contrib.auth.models import User
+from portfolio.constants import AssetTypes
+from .factories import (
+    UserFactory, 
+    PortfolioFactory, 
+    AssetFiiFactory, 
+    AssetStockFactory
+)
 
 class TestUtils():
 
     def get_standard_user(self):
-        return User.objects.get_or_create(
-            username='cleitonalmeida',
-            email='teste@cleitonalmeida.com.br',
-            password='teste'
-        )[0]
+        user = UserFactory()
+        return user
 
     def get_standard_portfolio(self, user: User):
-        return Portfolio.objects.get_or_create(
-            owner=user,
-            name='portfolio teste',
-        )[0]
+        portfolio = PortfolioFactory(owner=user)
+        return portfolio
 
     def get_standard_asset_type(self, name: str):
         return AssetType.objects.get_or_create(
             name=name)[0]
 
-    def get_standard_asset(self, ticker: str, type_investment: str):
-        asset_type = AssetType.objects.get_or_create(
-            name=type_investment)[0]
-        return Asset.objects.get_or_create(
-            ticker = ticker,
-            name = ticker,
-            type_investment = asset_type
-        )[0]
+    def get_standard_asset(self, type_investment: str):
+        facs = {
+            AssetTypes.FII.value: AssetFiiFactory,
+            AssetTypes.STOCK.value: AssetStockFactory,
+        }
+        
+        asset = facs[type_investment]()
+        return asset
